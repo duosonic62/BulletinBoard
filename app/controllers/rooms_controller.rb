96b8db9e442110ee6566375fec334970babf5ec4
Sekiguchi Riku <l.life.style.of.sustainability@gmail.com>
2019-02-07 16:16:17 +0900

    fix routesの修正
    routes.rbからnewを削除

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -24,27 +24,6 @@ class RoomsController < ApplicationController
     end
   end
 
-  def new
-    # ランダムな8~11文字程度のルームIDを割り振る
-    random_room_id = SecureRandom.urlsafe_base64(8)
-
-    # もしルームIDが被ったら新たにユニークなルームIDを発行する
-    while Room.exists?(room_id:random_room_id) do
-      random_room_id = SecureRandom.urlsafe_base64(8)
-    end
-
-    # ルームIDを発行する
-    @room = Room.new(room_id: random_room_id)
-
-    if @room.save
-      # indexをレンダリング(flashで新規に作成されたIDを渡す)
-      flash.now[:created_room_id] = @room.room_id
-      render 'index'
-    else
-      # エラーが発生した際はエラーメッセージをインデックスでレンダリング
-      flash.now[:alert] = "Room ID can't create. Try again."
-      render 'index'
-    end
-  end
+  
   
 end
